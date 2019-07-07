161f8402348fa5de21f8470fd8013451d7034c9e
sumithrajanagaraj <sumithrajanagarajm@gmail.com>
2019-07-07 15:10:44 +0530

    17 th file

---
+++ Multithread.java
@@ -0,0 +1,15 @@
+public class Multithread implements Runnable
+{
+       public static void main(String[] args) {
+        Thread guruThread1 = new Thread("Guru1");
+        Thread guruThread2 = new Thread("Guru2");
+        guruThread1.start();
+        guruThread2.start();
+        System.out.println("Thread names are following:");
+        System.out.println(guruThread1.getName());
+        System.out.println(guruThread2.getName());
+    }
+    public void run() {
+    }
+
+}
